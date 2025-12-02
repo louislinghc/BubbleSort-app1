2944ea8d72e23f5af045fe5bc8e06ef53dffbcae
louislinghc <linghchong@gmail.com>
2025-12-02 15:33:10 +0700

    initial commit

--- src/main/java/org/example/BubbleSort.java
+++ src/main/java/org/example/BubbleSort.java
@@ -6,13 +6,13 @@ public class BubbleSort {
         Integer[] a = arr.clone();
         int n = a.length;
         boolean swapped;
+
         for (int i = 0; i < n - 1; i++) {
             swapped = false;
             for (int j = 0; j < n - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
-                    int temp = a[j];
                     a[j] = a[j + 1];
-                    a[j + 1] = temp;
+                    a[j + 1] = a[j];
                     swapped = true;
                 }
             }
